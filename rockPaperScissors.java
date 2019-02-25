40422c321cfc4382111518b996f1ff7b6cd748cb
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-02-25 11:43:03 -0500

    Update rockPaperScissors.java

--- rockPaperScissors.java
+++ rockPaperScissors.java
@@ -7,8 +7,31 @@ public class rockPaperScissors
 		ArrayList<ternary> humanMoveHistory = new ArrayList<ternary>;
 		ArrayList<ternary> winHistory = new ArrayList<ternary>;
 	}
-	public static int ai(int[][] history) //-1 = rock, 0 = paper, 1 = scissors
+	public static ternary ai(ArrayList<ternary> winHistory, ArrayList<ternary> humanMoveHistory) //-1 = rock, 0 = paper, 1 = scissors
 	{
+		ternary output = new ternary;
+		if(winHistory.size() > 1)
+		{
+			ternary lastMove = humanMoveHistory.get(humanMoveHistry.size() - 1);
+			ternary moveBeforeThat = humanMoveHistory.get(humanMoveHistory.size() - 2);
+			if(lastMove.get() == moveBeforeThat.get() && winHistory.get(winHistory.size() - 1).get() != winHistory.get(winHistory.size() - 2).get())
+			{
+				output.set(lastMove.get()); //returns what would've lost to your last 2 moves (assumes you won't do it thrice)
+				output.sub();
+				return output.get();
+			}else if(winHistory.get(winHistory.size() - 1).get() == -1) //assumes user repeat and returns what would win against that
+			{
+				ouput.set(lastMove.get());
+				output.sub();
+				return output.get();
+			}
+		}else if(winHistory.size() == 1)
+		{
+			do stuff
+		}else if(winHistory.size() == 0)
+		{
+			do stuff
+		}
 		/*
 			Start with paper
 			check the past 2 moves (if same then play what would lose to that)
@@ -25,9 +48,10 @@ public class rockPaperScissors
 
 public class ternary //-1, 0, and 1
 {
+	Random random = new Random();
 	private int whatIsStored = 0;
 
-	public static void set(int i)
+	public static void set(int i) //stores a valid ternary value
 	{
 		if(i == -1 || i == 0 || i == 1)
 		{
@@ -38,27 +62,32 @@ public class ternary //-1, 0, and 1
 		}
 	}
 
-	public static void add()
+	public static void add() //adds 1, loops
 	{
 		whatIsStored ++;
 		if(whatIsStored == 2)
 			whatIsStored = -1;
 	}
 
-	public static void sub()
+	public static void sub() //subtracts 1, loops
 	{
 		whatIsStored --;
 		if(whatIsStored == -2)
 			whatIsStored = 1;
 	}
 
-	public static int get()
+	public static int get() //returns the stored value
 	{
 		return whatIsStored;
 	}
 
-	public static void inv()
+	public static void inv() //inverts the ternary (1 becomes -1, -1 becomes 1, 0 stays the same)
 	{
 		whatIsStored *= -1;
+	}
+
+	public static int ran() //returns a random valid ternary value
+	{
+		return random.nextInt(3) - 1;
 	}
 }
