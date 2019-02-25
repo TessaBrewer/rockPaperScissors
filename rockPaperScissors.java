d0dce7cea148c26a22ca38d393b0cf49c99f3993
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-02-25 10:52:54 -0500

    Add files via upload

---
+++ rockPaperScissors.java
@@ -0,0 +1,23 @@
+import Java.util.Scanner;
+public class rockPaperScissors
+{
+	public static void main(String [] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+	}
+	public static int ai(int[][] history) //0 = rock, 1 = paper, 2 = scissors
+	{
+		/*
+			Start with paper
+			check the past 2 moves (if same then play what would lose to that)
+			if lose then predict a repeat
+			if win then assume they assume a repeat
+			*** Impliment a NN ASAP ***
+		*/
+	}
+	public static int winLose(int human, int robot) //0 = lose, 1 = tie, 2 = win
+	{
+		
+	}
+}
