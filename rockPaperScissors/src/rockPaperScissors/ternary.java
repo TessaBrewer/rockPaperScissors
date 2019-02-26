880bee662beed52425ca39a5230936d200b472b1
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-02-26 07:55:08 -0500

    Update ternary.java

--- rockPaperScissors/src/rockPaperScissors/ternary.java
+++ rockPaperScissors/src/rockPaperScissors/ternary.java
@@ -1,5 +1,77 @@
 package rockPaperScissors;
+import java.util.*;
+public class ternary //-1, 0, and 1
+{
+	static Random random = new Random();
+	private static int whatIsStored = 0;
+	
+	public ternary(int start)
+	{
+		set(start);
+	}
 
-public class ternary {
+	public void set(int i) //stores a valid ternary value
+	{
+		if(i == -1 || i == 0 || i == 1)
+		{
+			whatIsStored = i;
+		}else
+		{
+			throw(new IllegalArgumentException("-1, 1, and 0 only"));
+		}
+	}
 
+	public void add() //adds 1, loops
+	{
+		whatIsStored ++;
+		if(whatIsStored == 2)
+			whatIsStored = -1;
+	}
+
+	public void sub() //subtracts 1, loops
+	{
+		whatIsStored --;
+		if(whatIsStored == -2)
+			whatIsStored = 1;
+	}
+
+	public int get() //returns the stored value
+	{
+		return whatIsStored;
+	}
+	
+	public int get(int offset)
+	{
+		if(offset > 0)
+		{
+			for(int i = 0; i < offset; i++)
+			{
+				whatIsStored++;
+				if(whatIsStored == 2)
+					whatIsStored = -1;
+			}
+			return whatIsStored;
+		}
+		if(offset < 0)
+		{
+			for(int i = 0; i > offset; i--)
+			{
+				whatIsStored--;
+				if(whatIsStored == -2)
+					whatIsStored = 1;
+			}
+			return whatIsStored;
+		}
+		return whatIsStored;
+	}
+
+	public void inv() //inverts the ternary (1 becomes -1, -1 becomes 1, 0 stays the same)
+	{
+		whatIsStored *= -1;
+	}
+
+	public static int rand() //returns a random valid ternary value
+	{
+		return random.nextInt(3) - 1;
+	}
 }
