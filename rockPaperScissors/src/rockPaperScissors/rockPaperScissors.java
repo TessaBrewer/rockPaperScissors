4c41763b46b6fc1c671006fdfc927a80f5fdf5d3
highqualitybean <32654356+highqualitybean@users.noreply.github.com>
2019-02-26 08:11:52 -0500

    Update rockPaperScissors.java

--- rockPaperScissors/src/rockPaperScissors/rockPaperScissors.java
+++ rockPaperScissors/src/rockPaperScissors/rockPaperScissors.java
@@ -2,17 +2,69 @@ package rockPaperScissors;
 import java.util.*;
 public class rockPaperScissors
 {
+	public static int humanWins = 0;
+	public static int robotWins = 0;
+	public static int ties = 0;
 	public static void main(String [] args)
-	{
+	{		
 		Scanner input = new Scanner(System.in);
 		ArrayList<ternary> humanMoveHistory = new ArrayList<ternary>();
 		ArrayList<ternary> winHistory = new ArrayList<ternary>();
 		System.out.println("0 quit\n1 rock\n2 paper\n3 scissor");
 		
-		ternary userChoice = new ternary(0);
-		userChoice.set(userInput(input));
-		ternary robot = ai(winHistory, humanMoveHistory);
-		winHistory.add(winLose(userChoice, robot));
+		while(true)
+		{
+			ternary userChoice = new ternary(0);
+			userChoice.set(userInput(input));
+			ternary robot = ai(winHistory, humanMoveHistory);
+			winHistory.add(winLose(userChoice, robot));
+			humanMoveHistory.add(userChoice);
+			System.out.print("\nYou: ");
+			
+			switch(userChoice.get())
+			{
+				case 1:
+					System.out.print("scissor");
+					break;
+				case 0:
+					System.out.print("paper");
+					break;
+				case -1:
+					System.out.print("rock");
+					break;
+			}
+			
+			System.out.print("\nThem: ");
+			
+			switch(robot.get())
+			{
+				case 1:
+					System.out.print("scissor");
+					break;
+				case 0:
+					System.out.print("paper");
+					break;
+				case -1:
+					System.out.print("rock");
+					break;
+			}
+			
+			switch(winLose(userChoice, robot).get())
+			{
+				case 1:
+					System.out.print("\nWin\n--------------------");
+					humanWins++;
+					break;
+				case 0:
+					System.out.print("\nTie\n--------------------");
+					ties++;
+					break;
+				case -1:
+					System.out.print("\nLose\n--------------------");
+					robotWins++;
+					break;
+			}
+		}
 	}
 
 	public static int userInput(Scanner input)
@@ -23,7 +75,7 @@ public class rockPaperScissors
 			throw(new IllegalArgumentException("Invalid input"));
 		}else if(userInput == 0)
 		{
-			System.exit(0);
+			quit();
 		}else
 		{
 			userInput -= 2;
@@ -86,5 +138,12 @@ public class rockPaperScissors
 			return output;
 		}
 		return output;
+	}
+	
+	public static void quit()
+	{
+		System.out.println("--------------------");
+		System.out.println(humanWins + ":" + ties + "robotWins");
+		System.exit(0);
 	}
 }
